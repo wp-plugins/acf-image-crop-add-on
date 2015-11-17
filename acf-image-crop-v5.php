916eed3860cddc0cfb8705a51df142ba6b69b883
andersthorborg <andersthorborg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-17 08:29:53 +0000

    Fix icons for ACF 5.3.2.2
    git-svn-id: https://plugins.svn.wordpress.org/acf-image-crop-add-on/trunk@1287741 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- acf-image-crop-v5.php
+++ acf-image-crop-v5.php
@@ -349,8 +349,8 @@ class acf_field_image_crop extends acf_field_image {
     </div>
     <div class="view show-if-value acf-soh">
         <ul class="acf-hl acf-soh-target">
-            <li><a class="acf-icon acf-icon-pencil dark" data-name="edit" href="#"><i class="acf-sprite-edit"></i></a></li>
-            <li><a class="acf-icon acf-icon-cancel dark" data-name="remove" href="#"><i class="acf-sprite-delete"></i></a></li>
+            <li><a class="acf-icon -pencil dark" data-name="edit" href="#"><i class="acf-sprite-edit"></i></a></li>
+            <li><a class="acf-icon -cancel dark" data-name="remove" href="#"><i class="acf-sprite-delete"></i></a></li>
         </ul>
         <img data-name="image" src="<?php echo $url; ?>" alt=""/>
         <div class="crop-section">
